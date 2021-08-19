01b2823e516c24b863038ee46c40e9d2780e65db
Luis Bardi <luisfbardi@gmail.com>
2021-08-18 22:24:17 -0300

    splash screen

--- lib/app/ui/pages/initial_page/initial_page.dart
+++ lib/app/ui/pages/initial_page/initial_page.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_app/app/ui/pages/login_page/login_page.dart';
+import 'package:splashscreen/splashscreen.dart';
 
 class InitialPage extends GetView<InitialController> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text('InitialPage')),
-        body: SafeArea(child: Text('InitialController')));
+    return Stack(
+      children: [
+        SplashScreen(
+          seconds: 5,
+          gradientBackground: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.green.shade300,
+              Colors.green.shade600,
+            ],
+          ),
+          navigateAfterSeconds: LoginPage(),
+          loaderColor: Colors.transparent,
+        ),
+        Container(
+          margin: EdgeInsets.all(140),
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/fire.png'),
+              fit: BoxFit.contain,
+            ),
+          ),
+        )
+      ],
+    );
   }
 }
