86843ff8ae77263fc5f3128d520324540605bc3e
Luis Bardi <luisfbardi@gmail.com>
2021-08-18 21:52:40 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+void main() {
+  runApp(
+    GetMaterialApp(
+      title: 'Login Firebase',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Login Firebase'),
+        ),
+      ),
+    ),
+  );
+}
